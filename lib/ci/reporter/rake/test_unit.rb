13038dac5e1a0caf857310459836a5fed3959a39
Nick <nick@nicksieger.com>
2007-09-26 01:43:52 +0000

    Change the way we add to the environment variables (courtesy Adam Anderson)
    git-svn-id: http://svn.caldersphere.net/svn/main/rubyforge/ci_reporter/trunk@157 b03c2d0b-2f10-0410-a2f9-fc8001506dfa

--- lib/ci/reporter/rake/test_unit.rb
+++ lib/ci/reporter/rake/test_unit.rb
@@ -6,8 +6,7 @@ namespace :ci do
   namespace :setup do
     task :testunit do
       rm_rf ENV["CI_REPORTS"] || "test/reports"
-      ENV["TESTOPTS"] ||= ""
-      ENV["TESTOPTS"] += " #{File.dirname(__FILE__)}/test_unit_loader.rb"
+      ENV["TESTOPTS"] = "#{ENV["TESTOPTS"]} #{File.dirname(__FILE__)}/test_unit_loader.rb"
     end
   end
 end
