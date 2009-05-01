9b8cc28438da6cd7281d166c0e6aadef84e06518
Graeme Mathieson <mathie@woss.name>
2009-05-01 13:55:43 +0100

    Add in a place to stick specs for the Cucumber integration.

---
+++ spec/ci/reporter/cucumber_spec.rb
@@ -0,0 +1,10 @@
+# (c) Copyright 2006-2008 Nick Sieger <nicksieger@gmail.com>
+# See the file LICENSE.txt included with the distribution for
+# software license details.
+
+require File.dirname(__FILE__) + "/../../spec_helper.rb"
+require 'stringio'
+
+describe "The Cucumber reporter" do
+  it "should work"
+end
