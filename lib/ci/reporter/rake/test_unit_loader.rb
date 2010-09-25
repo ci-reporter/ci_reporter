5d9ba3604f8b1f26b393b5b47107730bd10d24c6
amfranz <amfranz@gmail.com>
2010-09-24 20:09:59 -0700

    Adjusting the logic to install the CI::Reporter::TestUnit mediator so that it will work for newer ruby-test versions (>= 2.1) as well.

--- lib/ci/reporter/rake/test_unit_loader.rb
+++ lib/ci/reporter/rake/test_unit_loader.rb
@@ -1,3 +1,4 @@
+
 # Copyright (c) 2006-2010 Nick Sieger <nicksieger@gmail.com>
 # See the file LICENSE.txt included with the distribution for
 # software license details.
@@ -5,6 +6,7 @@
 $: << File.dirname(__FILE__) + "/../../.."
 require 'ci/reporter/test_unit'
 
+# Intercepts mediator creation in ruby-test < 2.1
 module Test #:nodoc:all
   module Unit
     module UI
@@ -14,6 +16,20 @@ module Test #:nodoc:all
             # swap in our custom mediator
             return CI::Reporter::TestUnit.new(suite)
           end
+        end
+      end
+    end
+  end
+end
+
+# Intercepts mediator creation in ruby-test >= 2.1
+module Test #:nodoc:all
+  module Unit
+    module UI
+      class TestRunner
+        def setup_mediator
+          # swap in our custom mediator
+          @mediator = CI::Reporter::TestUnit.new(@suite)
         end
       end
     end
