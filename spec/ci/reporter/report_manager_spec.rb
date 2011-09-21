6f00e20a20cbfe6073b67b627bd838288fac2085
Nick Sieger <nick@nicksieger.com>
2011-09-21 16:27:16 +0300

    Fix report manager spec long file name

--- spec/ci/reporter/report_manager_spec.rb
+++ spec/ci/reporter/report_manager_spec.rb
@@ -1,4 +1,4 @@
-# Copyright (c) 2006-2010 Nick Sieger <nicksieger@gmail.com>
+# Copyright (c) 2006-2011 Nick Sieger <nicksieger@gmail.com>
 # See the file LICENSE.txt included with the distribution for
 # software license details.
 
@@ -18,14 +18,14 @@ describe "The ReportManager" do
     CI::Reporter::ReportManager.new("spec")
     File.directory?(@reports_dir).should be_true
   end
-  
+
   it "should create the report directory based on CI_REPORTS environment variable if set" do
     @reports_dir = "#{Dir.getwd}/dummy"
     ENV["CI_REPORTS"] = @reports_dir
     CI::Reporter::ReportManager.new("spec")
     File.directory?(@reports_dir).should be_true
   end
-  
+
   it "should write reports based on name and xml content of a test suite" do
     reporter = CI::Reporter::ReportManager.new("spec")
     suite = mock("test suite")
@@ -43,7 +43,7 @@ describe "The ReportManager" do
     suite.should_receive(:name).and_return("some test suite name that goes on and on and on and on and on and on and does not look like it will end any time soon and just when you think it is almost over it just continues to go on and on and on and on and on until it is almost over but wait there is more and then el fin")
     suite.should_receive(:to_xml).and_return("<xml></xml>")
     reporter.write_report(suite)
-    filename = "#{REPORTS_DIR}/SPEC-some-test-suite-name-that-goes-on-and-on-and-on-and-on-and-on-and-on-and-does-not-look-like-it-will-end-any-time-soon-and-just-when-you-think-it-is-almost-over-it-just-continues-to-go-on.xml"
+    filename = "#{REPORTS_DIR}/SPEC-some-test-suite-name-that-goes-on-and-on-and-on-and-on-and-on-and-on-and-does-not-look-like-it-will-end-any-time-soon-and-just-when-you-think-it-is-almost-over-it-just-continues-t.xml"
     File.exist?(filename).should be_true
     File.open(filename) {|f| f.read.should == "<xml></xml>"}
   end
