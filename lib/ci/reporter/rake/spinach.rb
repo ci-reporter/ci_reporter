66b6a79446ecda9ee42d5dcf660abb27a5873ef8
Matt Burke <spraints@gmail.com>
2012-07-17 06:43:06 -0400

    Load spinach on demand.

--- lib/ci/reporter/rake/spinach.rb
+++ lib/ci/reporter/rake/spinach.rb
@@ -1,5 +1,3 @@
-#require File.expand_path('../utils', __FILE__)
-
 namespace :ci do
   namespace :setup do
     task :spinach_report_cleanup do
@@ -7,7 +5,15 @@ namespace :ci do
     end
 
     task :spinach => :spinach_report_cleanup do
-      # ???
+      loader = File.expand_path('prepare_ci_reporter.rb', ENV["SPINACH_SUPPORT_PATH"] || 'features/support')
+      if !File.exist? loader
+        File.open(loader, 'w') do |f|
+          f.puts "require 'ci/reporter/rake/spinach_loader'"
+        end
+        at_exit do
+          File.unlink loader
+        end
+      end
     end
   end
 end
