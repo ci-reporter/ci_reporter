8a6af9876314031b33d8e17224baf98ee47d41f5
Nick Sieger <nick@nicksieger.com>
2012-11-30 10:51:15 -0600

    Conditionally undef these methods

--- lib/ci/reporter/rake/test_unit_loader.rb
+++ lib/ci/reporter/rake/test_unit_loader.rb
@@ -11,7 +11,7 @@ module Test #:nodoc:all
     module UI
       module Console
         class TestRunner
-          undef :create_mediator
+          undef :create_mediator if instance_methods.map(&:to_s).include?("create_mediator")
           def create_mediator(suite)
             # swap in our custom mediator
             return CI::Reporter::TestUnit.new(suite)
@@ -27,7 +27,7 @@ module Test #:nodoc:all
   module Unit
     module UI
       class TestRunner
-        undef :setup_mediator
+        undef :setup_mediator if instance_methods.map(&:to_s).include?("setup_mediator")
         def setup_mediator
           # swap in our custom mediator
           @mediator = CI::Reporter::TestUnit.new(@suite)
