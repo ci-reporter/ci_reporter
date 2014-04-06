e86e54022aee8503890dec8bf2afd498c50327f1
Nick Sieger <nick@nicksieger.com>
2014-04-06 14:06:27 -0500

    v1.9.2

--- lib/ci/reporter/version.rb
+++ lib/ci/reporter/version.rb
@@ -1,11 +1,11 @@
 #--
-# Copyright (c) 2006-2013 Nick Sieger <nicksieger@gmail.com>
+# Copyright (c) 2006-2014 Nick Sieger <nicksieger@gmail.com>
 # See the file LICENSE.txt included with the distribution for
 # software license details.
 #++
 
 module CI
   module Reporter
-    VERSION = "1.9.1"
+    VERSION = "1.9.2"
   end
 end
