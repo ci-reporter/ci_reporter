c6b3576b667abd54d9f292d293996f6b27a38480
Jake Goulding <jake.goulding@gmail.com>
2014-06-08 15:52:51 -0400

    Remove trailing whitespace

--- lib/ci/reporter/report_manager.rb
+++ lib/ci/reporter/report_manager.rb
@@ -8,15 +8,15 @@ module CI #:nodoc:
         @basename = "#{@basedir}/#{prefix.upcase}"
         FileUtils.mkdir_p(@basedir)
       end
-      
+
       def write_report(suite)
         File.open(filename_for(suite), "w") do |f|
           f << suite.to_xml
         end
       end
-      
+
     private
-    
+
 
       # creates a uniqe filename per suite
       # to prevent results from being overwritten
@@ -35,16 +35,16 @@ module CI #:nodoc:
       def filename_for(suite)
         basename = "#{@basename}-#{suite.name.gsub(/[^a-zA-Z0-9]+/, '-')}"
         suffix = "xml"
-        
+
         # shorten basename if it exceeds 240 characters
         # most filesystems have a 255 character limit
         # so leave some room for the sidesteps
         max_filename_size = (ENV['MAX_FILENAME_SIZE'] || MAX_FILENAME_SIZE).to_i
         basename = basename[0..max_filename_size] if basename.length > max_filename_size
-        
+
         # the initial filename, e.g. SPEC-MailsController.xml
         filename = [basename, suffix].join(".")
-        
+
         # if the initial filename is already in use
         # do sidesteps, beginning with SPEC-MailsController.0.xml
         i = 0
@@ -52,7 +52,7 @@ module CI #:nodoc:
           filename = [basename, i, suffix].join(".")
           i += 1
         end
-        
+
         filename
       end
     end
